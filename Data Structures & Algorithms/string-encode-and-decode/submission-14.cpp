b4f4bbce1a51c88d6d325a57f53c2cb4b0dcae47
Himanshu Pokhriyal <145280742+hima-97435@users.noreply.github.com>
2026-05-03 01:32:12 +0530

    Add: string-encode-and-decode - submission-14

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-14.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+// final encoded string would be like 
+// #5hello#5world --> wrong
+    string encode(vector<string>& strs) {
+        string si = "";
+        for(string s : strs){
+            si+=to_string(s.size()) +"#"+s;
+        }
+        return si;
+
+    }
+
+    vector<string> decode(string s) {
+        vector<string> ans;
+        int i=0;
+        while(i<s.size()){
+            int j=i;
+            while(s[j]!='#') j++;
+            int len = stoi(s.substr(i,j-i));
+            i=j+1;
+            ans.push_back(s.substr(i,len));
+            i+=len;
+        }
+        return ans;
+    }
+};
