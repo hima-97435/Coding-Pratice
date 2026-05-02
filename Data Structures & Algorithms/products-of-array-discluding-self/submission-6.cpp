4afa1c02fb0eb024674224c17db2c29c0b76123a
Himanshu Pokhriyal <145280742+hima-97435@users.noreply.github.com>
2026-05-03 01:35:29 +0530

    Add: products-of-array-discluding-self - submission-6

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-6.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> ans ;
+        for( int i=0;i<nums.size();i++){
+            int sum =1;
+            for( int j=0;j<nums.size();j++){
+                if(i == j) continue;
+                sum*=nums[j];
+            }
+            ans.push_back(sum);
+        }
+        return ans;
+    }
+};
