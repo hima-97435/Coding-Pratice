4b22125f2a4803586ebb28db08fe01cdfcf8610e
Himanshu Pokhriyal <145280742+hima-97435@users.noreply.github.com>
2026-05-03 01:55:46 +0530

    Add: products-of-array-discluding-self - submission-8

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-8.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        // nums=[1,2,4,6] 
+        // prefix= [0,1,2,8] -->wrong
+        // suffix= [48,24,6,0] -->wrong
+        // prefix =[1,1,2,8] --> right
+        // suffix=[48,24,6,1] -->right
+        int n = nums.size();
+        vector<int> prefix (n);
+        // vector<int> suffix (n);
+        // vector<int> res(n);
+        prefix[0]=1;
+        // suffix[n-1]=1;
+        for( int i=1;i<n;i++) prefix[i]=prefix[i-1]*nums[i-1];
+        // for( int i=n-2;i>=0;i--) suffix[i]=suffix[i+1]*nums[i+1];
+        int suffix=1;
+        for( int i=n-1;i>=0;i--) {
+            
+            prefix[i]=prefix[i]*suffix;
+            suffix*=nums[i];            
+        }
+        return prefix;
+    }
+};
