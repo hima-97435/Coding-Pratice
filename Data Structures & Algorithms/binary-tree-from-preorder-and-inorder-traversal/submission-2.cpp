4f5e8e7a2c9f77fc67f204fe342ec4cee38e2e18
Himanshu Pokhriyal <145280742+hima-97435@users.noreply.github.com>
2026-05-20 13:48:52 +0530

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-2

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-2.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+       unordered_map<int , int> inordermap;
+       for( int i=0;i<inorder.size();i++){
+        inordermap[inorder[i]]=i;
+
+       }
+
+       return helper(preorder,0,preorder.size()-1, inorder, 0, inorder.size()-1, inordermap);
+        
+    }
+    TreeNode* helper(vector<int> & preorder , int ps, int pe , vector<int>&inorder , int is , int ie, unordered_map<int , int>& inordermap){
+        if(ps>pe || is>ie) return nullptr;
+        TreeNode* node = new TreeNode(preorder[ps]);
+        int index = inordermap[preorder[ps]];
+        
+        int leftsize= index - is;
+        node->left= helper(preorder, ps+1, ps+leftsize, inorder, is, index-1,inordermap);
+        node->right= helper(preorder, ps+leftsize+1, pe, inorder, index+1, ie,inordermap);
+        return node;
+    }
+};
