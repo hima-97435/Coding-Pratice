3568f69dfc2d587ce40bc5b58a25ea4eaba884f5
Himanshu Pokhriyal <145280742+hima-97435@users.noreply.github.com>
2026-05-29 11:54:31 +0530

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.cpp
@@ -0,0 +1,25 @@
+
+class WordDictionary {
+    vector<string> store;
+public:
+    WordDictionary() {
+        
+    }
+    
+    void addWord(string word) {
+        store.push_back(word);
+    }
+    
+    bool search(string word) {
+        for(string w: store){
+            if(w.length() != word.length()) continue;
+            int i=0;
+            while(i<w.length()){
+                if(w[i] == word[i] || word[i] == '.') i++;
+                else break;
+            }
+            if( i == w.length()) return true;
+        }
+        return false;
+    }
+};
