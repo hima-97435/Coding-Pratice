306354e57b7fc752e09c6952e7deabf5596023fe
Himanshu Pokhriyal <145280742+hima-97435@users.noreply.github.com>
2026-06-10 14:13:22 +0530

    Add: coin-change - submission-5

---
+++ Data Structures & Algorithms/coin-change/submission-5.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+         vector<int> dp(amount+1, amount+1);
+         dp[0]=0;
+         for( int i=1;i<=amount;i++){
+            for( int x: coins){
+                if(i-x>=0){
+                    dp[i]= min(dp[i], 1+ dp[i-x]);
+                }
+            }
+         }
+        for( int x: dp) cout<<x<<" ";
+        cout<<endl;
+         return dp[amount] > amount ? -1: dp[amount];
+
+    }
+};
