e7c4f0be32dbda50b4d193d51a4c0d196f87d904
Himanshu Pokhriyal <145280742+hima-97435@users.noreply.github.com>
2026-07-04 14:38:20 +0530

    Add: continuous-subarray-sum - submission-3

---
+++ Data Structures & Algorithms/continuous-subarray-sum/submission-3.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        for(int i=0;i<n-1;i++){
+            int sum = nums[i];
+            for(int j=i+1;j<n;j++){
+                sum+=nums[j];
+                if(sum%k == 0) return true;
+            }
+        }
+        return false;
+    }
+};
