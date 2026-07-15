240501b7a75bddd7cd289a756f601c09d6485288
Himanshu Pokhriyal <145280742+hima-97435@users.noreply.github.com>
2026-07-15 14:06:01 +0530

    Add: length-of-last-word - submission-0

---
+++ Data Structures & Algorithms/length-of-last-word/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.size();
+        int i=n-1, length =0;
+        while(s[i] == ' ' ) i--;
+        while(i>=0 && s[i] != ' ') {i--; length++;}
+        return length;
+    }
+};
