5c9f89919a760a14f23fbefae883d368f3b693c7
Himanshu Pokhriyal <145280742+hima-97435@users.noreply.github.com>
2026-07-15 17:55:46 +0530

    Add: maximum-profit-in-job-scheduling - submission-0

---
+++ Data Structures & Algorithms/maximum-profit-in-job-scheduling/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+vector<vector<int>> intervals;
+vector<int> cache;
+
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        int n = startTime.size();
+        intervals.resize(n, vector<int>(3));
+        cache.assign(n, -1);
+        for(int i=0;i<n;i++){
+            intervals[i]={startTime[i], endTime[i], profit[i]};
+        }        
+        sort(intervals.begin(), intervals.end());
+        auto dfs =[&] (auto&&self, int i) -> int{
+            if( i == n) return 0;
+            if(cache[i] != -1) return cache[i];
+            int res = self(self,i+1);
+            int j=i+1;
+            while(j<n && intervals[i][1]>intervals[j][0]) j++;
+            return cache[i]=max(res,intervals[i][2]+self(self,j));
+        };
+        return dfs(dfs,0);
+    }
+};
