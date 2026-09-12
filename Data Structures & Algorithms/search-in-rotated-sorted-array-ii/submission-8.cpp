dfad91c2a89418558070648d31ed62176eb645e2
Himanshu Pokhriyal <145280742+hima-97435@users.noreply.github.com>
2026-09-12 19:10:42 +0530

    Add: search-in-rotated-sorted-array-ii - submission-8

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-8.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+         int l =0 , r= nums.size()-1;
+        while(l<=r){
+            int m = l+(r-l)/2;
+            if(nums[m] == target) return true;
+            else {
+                if(nums[l] == nums[m]) l++;
+                else if(nums[l]<nums[m]) {
+                    if(target>=nums[l] && target<=nums[m]) r=m;
+                    else l=m+1;
+                }
+                else {
+                    if(target>=nums[m] && target<=nums[r]) l=m;
+                    else r=m-1;
+                }
+            }
+        }
+        return false;
+    
+    }
+};
