152b32a9a5c631b136e521477964ca7bf63c4767
Himanshu Pokhriyal <145280742+hima-97435@users.noreply.github.com>
2026-09-18 15:04:48 +0530

    Add: russian-doll-envelopes - submission-7

---
+++ Data Structures & Algorithms/russian-doll-envelopes/submission-7.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        sort(envelopes.begin(),envelopes.end(), [](auto&a, auto&b){
+            if(a[0]!= b[0]) return a[0]<b[0];
+            return a[1]>b[1];
+        });
+        int n = envelopes.size();
+        vector<int> nums(n);
+        for(int i=0;i<n;i++) nums[i]=envelopes[i][1];
+        int lis =1;
+        vector<int> dp ={nums[0]};
+        for(int i=1;i<n;i++){
+            if(dp.back()<nums[i]) {
+                dp.push_back(nums[i]);
+                lis++;
+                continue;
+            }
+            int index = lower_bound(dp.begin(),dp.end(),nums[i])-dp.begin();
+            dp[index]=nums[i];
+        }
+        return lis;
+    }
+};
